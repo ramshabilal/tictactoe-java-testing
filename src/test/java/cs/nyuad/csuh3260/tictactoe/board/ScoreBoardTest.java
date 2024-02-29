0afd101080b94281c2a00e26a4733d5cc40ce71c
Ramsha Bilal <rb4919@nyu.edu>
2024-03-01 00:57:02 +0400

    added branch and initial setup for scoreboard testing

---
+++ src/test/java/cs/nyuad/csuh3260/tictactoe/board/ScoreBoardTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ScoreBoardTest {
+
+    private ScoreBoard scoreboard;
+
+    @BeforeEach
+    public void setUp() {
+        scoreboard = new ScoreBoard();
+    }
+}
